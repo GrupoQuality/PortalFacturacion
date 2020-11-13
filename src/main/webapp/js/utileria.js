e944717c9b7c7b18546d03ffc41f38449fbb9970
'erick' <'aguilar505088@gmail.com'>
2020-11-12 20:32:16 -0600

    Ya obtenemos el folio fiscal de cualquier factura en la tabla y generamos el pdf, la mitad del pdf ya es informacion real de la  base de datos, pero lo demas no lo hemos acabado

--- src/main/webapp/js/utileria.js
+++ src/main/webapp/js/utileria.js
@@ -1,13 +1,14 @@
     var tabla=null;
+
       function get()
-    {        
+    {   
         console.log("entro a get")
         if(tabla!=null){tabla.destroy();}
             tabla=$('#tabla-facturas').DataTable( {searching: false,
                 "ajax": "consultarFacturas.do",
                 "columns": [{"render": function () {
                            return '<input type="checkbox">'}},
-                    {data: "Folio Fiscal" },
+                    {data: "Folio_Fiscal"},
                     {data: "RFC Emisor"},
                     {data: "RFC del cliente"},
                     {data: "Nombre del cliente"},
@@ -16,18 +17,42 @@
                     {data: "Total"},
                     {data: "Estado"},
                     {"render": function () {
-                           return '<i class="icon-doc-inv"></i>'}},
+                           return '<i class="icon-doc-inv" id = "" onclick = ""></i>'}},
                     {"render": function () {
-                           return '<button class="boton-table">PDF</button>\n\
+                           return '<button class="boton-table" onclick = "">PDF</button>\n\
 <button class="boton-table">XML</button>'}},
                 ]
         } );
         const table = document.getElementById('tabla-facturas');
         table.style.setProperty('width',`${100}%`)
+        validar()
     }
     
-    function generatePdf(){
+    function validar(){
+        let vfolio = ""
+        var myTable = $('#tabla-facturas').DataTable();
+        myTable.on('click','i',function() {
+            var rowData = myTable.row($(this).parents('tr')).data();            
+            vfolio = rowData.Folio_Fiscal
+//            alert(typeof vfolio)    
+            if(vfolio == ""){
+
+            }
+            else {
+//                alert("antes de pdf: " + vfolio)
+                generatePdf(vfolio)
+                const openModal = document.getElementById("open-modal-pdf");
+                const pdfObjectContainer = document.getElementById("object-pdf-container");
+//                pdfObjectContainer.setAttribute('data','${pageContext.request.contextPath}/pdfs/pdf.pdf')
+                setTimeout(()=>{openModal.click()},2000)                
+            }
+        });                
+    }
+    
+   
+    function generatePdf(fFiscal){
         let fc_factura = "esto es un ejemplo para hacer el pdf"
+        fc_factura = fFiscal
         $.ajax({
             url : 'generarPdf.do',
             type : "POST",
@@ -35,7 +60,7 @@
             beforeSend : function(xhr) {
             },
             success : function(jsonResponse, textStatus, jqXHR) {
-                alert("listo");
+//                alert("listo");
             },
             error : function(jqXHR, textStatus, errorThrown) {
 //                console.log(textStatus);
